815a5f1ee35bc5c7a062350bc04540e39bacd68c
Leridy <leiyudibiz@gmail.com>
2024-11-07 13:08:44 +0800

    update fonts

--- src/app/api/image/validationCode/route.ts
+++ src/app/api/image/validationCode/route.ts
@@ -83,11 +83,11 @@ const generateValidateCodeImage = (code: string) => {
         font-weight: 100 900;
         font-stretch: 100%;
         font-display: swap;
-        src: url(https://fonts.gstatic.com/s/sourgummy/v1/8At7Gs2gPYuNDii97MjjLLvpghcw76OXBoIHpHgGTN1IOL9FKB11Og.woff2) format('woff2');
+        src: local("Noto Sans");
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }
       .text {
-        font-family: 'Sour Gummy';
+        font-family: 'Noto Sans';
       }
     </style>
   `
