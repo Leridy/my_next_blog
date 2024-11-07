cac3d89275b128919d5ee3e0d676b8ee4c751aa4
Leridy <leiyudibiz@gmail.com>
2024-11-07 12:50:54 +0800

    feat: update fonts

--- src/app/api/image/validationCode/route.ts
+++ src/app/api/image/validationCode/route.ts
@@ -83,7 +83,7 @@ const generateValidateCodeImage = (code: string) => {
   font-weight: 100 900;
   font-stretch: 100%;
   font-display: swap;
-  src: url(https://fonts.gstatic.com/s/sourgummy/v1/8At7Gs2gPYuNDii97MjjLLvpghcw76OXBoIHpHgGTN1IOL9FKB11Og.woff2) format('woff2');
+  src: url(/fonts/8At7Gs2gPYuNDii97MjjLLvpghcw76OXBoIHpHgGTN1IOL9FKB11Og.woff2) format('woff2');
   unicode-range: U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
 }
 /* latin */
@@ -93,7 +93,7 @@ const generateValidateCodeImage = (code: string) => {
   font-weight: 100 900;
   font-stretch: 100%;
   font-display: swap;
-  src: url(https://fonts.gstatic.com/s/sourgummy/v1/8At7Gs2gPYuNDii97MjjLLvpghcw76OXBoIHpHgGTNNIOL9FKB0.woff2) format('woff2');
+  src: url(/fonts/8At7Gs2gPYuNDii97MjjLLvpghcw76OXBoIHpHgGTNNIOL9FKB0.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
 /* latin-ext */
@@ -103,7 +103,7 @@ const generateValidateCodeImage = (code: string) => {
   font-weight: 100 900;
   font-stretch: 100%;
   font-display: swap;
-  src: url(https://fonts.gstatic.com/s/sourgummy/v1/8At5Gs2gPYuNDii97MjjBrLbYfdJvDU5AZfP5qBNfNFCP51HGBw.woff2) format('woff2');
+  src: url(/fonts/8At5Gs2gPYuNDii97MjjBrLbYfdJvDU5AZfP5qBNfNFCP51HGBw.woff2) format('woff2');
   unicode-range: U+0100-02BA, U+02BD-02C5, U+02C7-02CC, U+02CE-02D7, U+02DD-02FF, U+0304, U+0308, U+0329, U+1D00-1DBF, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
 }
 /* latin */
@@ -113,7 +113,7 @@ const generateValidateCodeImage = (code: string) => {
   font-weight: 100 900;
   font-stretch: 100%;
   font-display: swap;
-  src: url(https://fonts.gstatic.com/s/sourgummy/v1/8At5Gs2gPYuNDii97MjjBrLbYfdJvDU5AZfP5qBDfNFCP51H.woff2) format('woff2');
+  src: url(/fonts/8At5Gs2gPYuNDii97MjjBrLbYfdJvDU5AZfP5qBDfNFCP51H.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
       .text {
