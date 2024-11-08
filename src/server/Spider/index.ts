5b9e7deb28c4b03ad5f0804d75a8970d57bf50a5
Leridy <leiyudibiz@gmail.com>
2024-11-08 11:55:58 +0800

    feat: update logic of spider

--- src/server/Spider/index.ts
+++ src/server/Spider/index.ts
@@ -7,14 +7,14 @@ import {MyNRError} from "@/utils/MyNRError";
 
 export default async function Spider() {
   try {
-    const tasks = [kr(), ithome(), hupu(), bilibili(), juejin()];
+    const tasks = [kr, ithome, hupu, bilibili, juejin];
     const updateResult = [];
     // 为什么不用 Promise.all 呢？因为 Promise.all 会在其中一个任务出错时就直接 reject，而我们希望所有任务都执行完，然后返回所有结果
     // 还有一个原因是，我们希望在出错时，能够知道是哪个任务出错了，所以我们需要一个一个任务的执行
     // 边缘计算性能较差，所以我们需要担心并发过高的问题，所以我们需要一个一个任务的执行
     for (let i = 0; i < tasks.length; i++) {
       try {
-        const result = await tasks[i];
+        const result = await tasks[i]();
         updateResult.push(result);
       } catch (e) {
         if (e instanceof MyNRError) throw e;
