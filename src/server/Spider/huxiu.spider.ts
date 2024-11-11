97ab2e2624b85185e994b47bdd363abf52ba4846
Leridy <leiyudibiz@gmail.com>
2024-11-11 23:14:06 +0800

    feat: update huxiu part and spider login

---
+++ src/server/Spider/huxiu.spider.ts
@@ -0,0 +1,128 @@
+import http from "./http";
+import {HotNews, HotSpider} from "@prisma/client";
+import {checkAndOperateNews, spiderPublicLogic, updateSpiderUpdateTime} from "@/server/Spider/utils/spiderPublicLogic";
+
+interface HuxiuDataStructure {
+  object_type: number;
+  object_id: number;
+  content: string;
+  publish_time: number;
+  url: string;
+  comment_status: number;
+  is_agree: boolean;
+  is_favorite: boolean;
+  status: number;
+  is_hot: boolean;
+  type: number;
+  agree_icon: string;
+  count_info: {
+    total_comment_num: number;
+    agree_num: number;
+    favorite_num: number;
+    reward_num: number;
+  }
+  user_info: {
+    uid: number;
+    username: string;
+    avatar: string;
+    yijuhua: string;
+    user_icons: {
+      name: string;
+      order: string;
+      type: number;
+      icon_path: string;
+      icon_night_path: string;
+      position_id: number;
+    }[]
+    ip_url: string;
+  }
+  reward_status: number;
+  is_reward: boolean;
+  is_follow: boolean;
+  comment: {
+    total: number;
+    total_page: number;
+    cur_page: number;
+    datalist: {
+      comment_id: number;
+      parent_comment_id: number;
+      to_comment_id: number;
+      object_type: number;
+      object_id: number;
+      user_info: {
+        uid: number;
+        username: string;
+      },
+      content: string;
+      region: string;
+    }[]
+  };
+  format_time: string;
+  is_allow_edit_reward: boolean;
+  is_allow_share: boolean;
+}
+
+const SPIDER_INFO: Pick<HotSpider, 'name' | 'description'> = {
+  name: 'huxiu',
+  description: 'huxiu 爬虫',
+}
+
+const URL_GENERATOR = () => `https://www.huxiu.com/moment/`
+
+
+// 标题处理
+const titleProcessing = (text: string) => {
+  const paragraphs = text.split("<br><br>");
+  const title = paragraphs?.shift()?.replace(/。$/, "") || "";
+  const intro = paragraphs.join("<br><br>");
+  return {title, intro};
+};
+
+
+async function getData(): Promise<string> {
+  const url = URL_GENERATOR();
+  return await http.get(url, {});
+}
+
+function dataTransformer(data: HuxiuDataStructure[], spiderId: number): Pick<HotNews, 'title' | 'url' | 'description' | 'image' | 'spiderId' | 'uniqueId'>[] {
+  return data.filter(item => Boolean(item.url)).map((item, index) => {
+    return {
+      title: titleProcessing(item.content).title,
+      description: titleProcessing(item.content).intro,
+      image: item.user_info.avatar || '',
+      url: item.url || `https://www.huxiu.com/moment?from=pc&index=${index}`,
+      uniqueId: `huxiu-${item.object_id}`,
+      spiderId,
+      hotCount: item.count_info.agree_num + item.count_info.total_comment_num || 0,
+      tags: [item.user_info.username]
+    }
+  });
+}
+
+/**
+ * main logic of getData from 36kr
+ */
+export default async function main() {
+  const {id} = await spiderPublicLogic(SPIDER_INFO);
+
+  const pattern = /<script>[\s\S]*?window\.__INITIAL_STATE__\s*=\s*(\{[\s\S]*?\});[\s\S]*?<\/script>/;
+
+
+  const requestedData: string = await getData();
+
+  const matchResult = requestedData.match(pattern);
+  const result = JSON.parse(<string>matchResult?.[1]).moment.momentList.moment_list.datalist;
+
+  const transformedData = dataTransformer(result, id);
+
+
+  // data transform to your own format
+  await checkAndOperateNews(transformedData);
+  // update spider update time
+  await updateSpiderUpdateTime(id);
+
+
+  return transformedData;
+}
+
+
--- src/server/Spider/index.ts
+++ src/server/Spider/index.ts
@@ -6,31 +6,35 @@ import juejin from "@/server/Spider/juejin.spider";
 import v2ex from "@/server/Spider/v2ex.spider";
 import toutiao from "@/server/Spider/toutiao.spider";
 import zhihu from "@/server/Spider/zhihu.spider";
+import huxiu from "@/server/Spider/huxiu.spider";
 import {MyNRError} from "@/utils/MyNRError";
 
 export default async function Spider() {
   try {
-    // const tasks = [zhihu()]; // for test
-    const tasks = [kr, ithome, hupu, bilibili, juejin, v2ex, toutiao, zhihu];
-    const updateResult = [];
+    // const tasks = [huxiu()]; // for test
+    const tasks = [kr, ithome, hupu, bilibili, juejin, v2ex, toutiao, zhihu, huxiu];
+    // const updateResult = [];
     // 为什么不用 Promise.all 呢？因为 Promise.all 会在其中一个任务出错时就直接 reject，而我们希望所有任务都执行完，然后返回所有结果
     // 还有一个原因是，我们希望在出错时，能够知道是哪个任务出错了，所以我们需要一个一个任务的执行
     // 边缘计算性能较差，所以我们需要担心并发过高的问题，所以我们需要一个一个任务的执行
     for (let i = 0; i < tasks.length; i++) {
       try {
         // const result = await tasks[i]; // for test
-        const result = tasks[i]() ;
-        updateResult.push(result);
+
+        // 为了避免并发过高，我们需要设置一个间隔时间
+        setTimeout(() => {tasks[i]()}, 10000 * i);
+
+        // updateResult.push(result || {});
+
       } catch (e) {
         if (e instanceof MyNRError) throw e;
         if (e instanceof Error) {
           console.error(e);
           throw new MyNRError(`爬虫出错 ${e.message}`, 500, {originError: e});
         }
-
       }
     }
-    return {state: 'success', data: updateResult, message: '已经成功触发爬虫，正在爬取数据'};
+    return {state: 'success', message: '已经成功触发爬虫，正在爬取数据'};
     // return updateResult;
   } catch (e) {
     if (e instanceof MyNRError) throw e;
