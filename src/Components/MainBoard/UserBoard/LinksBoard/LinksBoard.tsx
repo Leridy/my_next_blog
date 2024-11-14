bf894bb077ee16db9c6ad1d5441d4c54422f3427
Leridy <leiyudibiz@gmail.com>
2024-11-15 02:08:56 +0800

    feat: update link panel

---
+++ src/Components/MainBoard/UserBoard/LinksBoard/LinksBoard.tsx
@@ -0,0 +1,57 @@
+import Card from "@/Components/Card";
+import useSettingMap from "@/Components/hooks/useSettingMap";
+import {useSiteSettingContext} from "@/Provider/SiteSettingProvider";
+import {useMemo} from "react";
+import {Tooltip} from "antd";
+
+const SITE_SETTING_KEY = 'UserBoard.LinksBoard';
+
+export default function LinksBoard() {
+  const {setting} = useSiteSettingContext();
+
+  const {links} = useSettingMap<{ links: string }>({
+    baseKey: SITE_SETTING_KEY,
+    setting,
+    subKeys: {
+      links: '🪨 网站里程碑 - https://docs.qq.com/doc/DTU5GbEVFTURpT05i\n🙏 特别鸣谢 - https://docs.qq.com/doc/DTVRYd0VXSmV5a2Va'
+    }
+  })
+
+  const linkList = useMemo(() => {
+    return String(links)?.split(';\n').map(link => {
+      const [title, url, desc] = link.split(' - ');
+      return {title, url, desc};
+    })
+  }, [links]);
+
+  const renderLinks = useMemo(() => {
+    return linkList.map(({title, url, desc}) => {
+      return (
+        <Tooltip
+          title={desc}
+          key={title}
+        >
+          <a
+            href={url}
+            rel={'noreferrer'}
+            target={'_blank'}
+          >{title}</a>
+        </Tooltip>
+
+      )
+    })
+  }, [linkList])
+
+  return (
+    <Card
+      header={'Links'}
+    >
+      <div
+        className={'grid gap-4 grid-cols-3'}
+      >
+        {renderLinks}
+      </div>
+    </Card>
+
+  )
+}
--- src/Components/MainBoard/UserBoard/UserBoard.tsx
+++ src/Components/MainBoard/UserBoard/UserBoard.tsx
@@ -2,6 +2,7 @@
 import Card from "../../Card";
 import UserProfile from "@/Components/MainBoard/UserBoard/UserProfile";
 import TipsAndNotification from "@/Components/MainBoard/UserBoard/TipsAndNotification/TipsAndNotification";
+import LinksBoard from "@/Components/MainBoard/UserBoard/LinksBoard/LinksBoard";
 // 用来获取配置信息的东西
 // const SITE_SETTING_KEY = 'UserBoard';
 /**
@@ -34,6 +35,8 @@ export default function UserBoard() {
       <Card header={'Tool'}>
         todo 做一个 todo list
       </Card>
+
+      <LinksBoard />
     </div>
 
   )
