0cc684159beb60c78e6f0f5bd82abbb9448c3726
Leridy <leiyudibiz@gmail.com>
2024-11-19 16:01:48 +0800

    feat: update the config

--- src/server/Spider/http/index.ts
+++ src/server/Spider/http/index.ts
@@ -1,4 +1,16 @@
-import axios, {AxiosError, AxiosResponse} from 'axios';
+import axios, {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
+
+const requestHandler = (request: AxiosRequestConfig): AxiosRequestConfig => {
+  // 在 headers 里面添加 user-agent the request possibly undefined
+  request.headers = request.headers || {};
+  request.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
+  // make spider http more like a browser
+  request.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8';
+  request.headers['Accept-Encoding'] = 'gzip, deflate, sdch';
+  request.headers['Accept-Language'] = 'zh-CN,zh;q=0.8,en;q=0.6';
+
+  return request;
+}
 
 const responseHandler = (response: AxiosResponse): AxiosResponse["data"] => {
   // add response handler
@@ -22,7 +34,7 @@ const errorHandler = (error: AxiosError) => {
 // create an axios instance with handler above
 const instance = axios.create({});
 
-instance.interceptors.request.use();
+instance.interceptors.request.use(requestHandler);
 instance.interceptors.response.use(responseHandler, errorHandler);
 
 export default instance;
